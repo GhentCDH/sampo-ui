42f002216c5d5531e422bd20c4a36bc3959ee830
Annastiina Ahola <anna.s.ahola@gmail.com>
2026-06-30 16:55:35 +0300

    Show spinner on InstancePageTables when fetching data

--- client/src/components/facet_results/ResultClassRoute.js
+++ client/src/components/facet_results/ResultClassRoute.js
@@ -140,7 +140,8 @@ const ResultClassRoute = props => {
         resultClass,
         fetchResults: props.fetchResults,
         properties,
-        screenSize
+        screenSize,
+        fetching: perspectiveState.fetching
       }
       if (resultClassConfig.fetchResultsWhenMounted) {
         instanceTableProps = {
